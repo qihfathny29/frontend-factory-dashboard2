f21c574b6f1ae985ce500b83b59b36e5a100f33f
Zedts <savana.cell152@gmail.com>
2025-12-02 09:24:29 +0700

    Remove Unused Code

--- src/Components/Dropdowns/index.ts
+++
@@ -1,4 +0,0 @@
-export { DropdownCompanies } from "./DropdownCompanies";
-export { DropdownPlant } from "./DropdownPlant";
-export { DropdownType } from "./DropdownType";
-export { DropdownPeriod } from "./DropdownPeriod";
